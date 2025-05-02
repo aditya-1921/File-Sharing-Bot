2fbe6ae2071137b82c63259d42ab2d996cc94f75
aditya-1921 <poisondeath1921@gmail.com>
2025-05-02 14:35:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,9 +65,9 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/seasonal_culture")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \n https://t.me/seasonal_culture")
         print(ascii_art)
-        print("""Welcome to CodeXBotz File Sharing Bot""")
+        print("""Welcome to Seasonal File Sharing Bot""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
